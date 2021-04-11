d7e1bb93c000f15d0eb23f3d0858e7bb9c107c24
Charlie Salinetti <cws2525@rit.edu>
2021-04-10 21:10:19 -0400

    added to get_credentials

--- current_song_player.py
+++ current_song_player.py
@@ -1,18 +1,10 @@
-from info import *
 from get_credentials import get_credentials
 from get_song import get_current_track
 from get_song import loop_current_track
 
 def play_current():
-    test = input("!!!Enter test for testing!!! ")
-
-    if (test == "test"):
-        access_token = SPOTIFY_ACCESS_TOKEN
-        url = SPOTIFY_GET_CURRENT_TRACK_URL
-    else:
-        access_token, url = get_credentials()
-    
-    loop_current_track(access_token, url )
+    access_token, url = get_credentials()  
+    loop_current_track(access_token, url)
 
 def main():
     play_current()
--- get_credentials.py
+++ get_credentials.py
@@ -1,6 +1,26 @@
+from client_id import *
+import webbrowser
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
 def get_credentials():
-    access_token = input("Enter Access Token: ")
-    username = input("Enter Username: ")
-    url = 'https://api.spotify.com/v1/'+ username +'/player/currently-playing'
+
+    webbrowser.open('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +'&response_type=code&redirect_uri=https://api.spotify.com/v1/me/player/currently-playing&scope=user-read-currently-playing', new = 2)
+
+
+
+
+
+
+
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+    auth_response_data = auth_response.json()
+    access_token = auth_response_data['access_token']
+    print(access_token)
+    url = 'https://api.spotify.com/v1/me/player/currently-playing'
 
     return access_token, url
