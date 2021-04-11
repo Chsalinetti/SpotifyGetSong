8467035c97b886a258bc2492dd608f76a879d85b
Charlie Salinetti <cws2525@rit.edu>
2021-04-11 16:08:14 -0400

    able to get code

--- get_credentials.py
+++ get_credentials.py
@@ -1,13 +1,22 @@
 from client_id import *
+import webbrowser
 import requests
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 def get_credentials():
-    response = requests.get('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +'&response_type=code&redirect_uri=https://api.spotify.com/v1/me/player/currently-playing&scope=user-read-currently-playing')
-    print(response)
-    print(response.text)
-    access_token = input()
+
+    webbrowser.open('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +'&response_type=code&redirect_uri=https://api.spotify.com/v1/me/player/currently-playing&scope=user-read-currently-playing')
+    cred_url = input("Enter URL: ")
+    tokens = cred_url.split('=')
+    code = tokens[1]
+    
+    
+
+
+
     url = 'https://api.spotify.com/v1/me/player/currently-playing'
 
+
+    access_token = 0
     return access_token, url
