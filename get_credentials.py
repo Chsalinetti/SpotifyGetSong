7485032d9cc8a2e951be5a870454c79e11425271
Charlie Salinetti <cws2525@rit.edu>
2021-04-11 15:31:01 -0400

    updated get_credentials

--- get_credentials.py
+++ get_credentials.py
@@ -1,26 +1,13 @@
 from client_id import *
-import webbrowser
+import requests
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 def get_credentials():
-
-    webbrowser.open('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +'&response_type=code&redirect_uri=https://api.spotify.com/v1/me/player/currently-playing&scope=user-read-currently-playing', new = 2)
-
-
-
-
-
-
-
-    auth_response = requests.post(AUTH_URL, {
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    })
-    auth_response_data = auth_response.json()
-    access_token = auth_response_data['access_token']
-    print(access_token)
+    response = requests.get('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +'&response_type=code&redirect_uri=https://api.spotify.com/v1/me/player/currently-playing&scope=user-read-currently-playing')
+    print(response)
+    print(response.text)
+    access_token = input()
     url = 'https://api.spotify.com/v1/me/player/currently-playing'
 
     return access_token, url
